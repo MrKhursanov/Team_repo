1619331a76e44017bb01516e013ce7bed360c8f9
MrKhursanov <khursanovnazarbek321@gmail.com>
2025-04-03 22:00:53 +0900

    Nazarbek added GCD.py files

---
+++ GCD - Copy.py
@@ -0,0 +1,9 @@
+def GCD(a,b):
+    while b:
+        a,b = b, a%b
+    return a # using Euclidean Algorithm
+
+print('hi, world') 
+
+
+print(GCD(8,4))
--- GCD.py
+++ GCD.py
@@ -2,6 +2,8 @@ def GCD(a,b):
     while b:
         a,b = b, a%b
     return a # using Euclidean Algorithm
-    
+
+print('hi, world') 
+
 
 print(GCD(8,4))
